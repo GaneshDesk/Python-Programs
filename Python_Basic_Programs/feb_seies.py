ee9ec3690e81cee88474693942c9373fdbfdc854
GaneshDesk <koradeganesh.r@gmail.com>
2020-08-18 19:55:31 +0530

    Basic python programs

---
+++ Python_Basic_Programs/feb_seies.py
@@ -0,0 +1,20 @@
+"""This program demonstrate fibonacce series """
+
+def fib(n):  # write Fibonacci series up to n
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+        print()
+
+def fib2(n): # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    print(result)
+
+
+fib(20)
+fib2(20)
