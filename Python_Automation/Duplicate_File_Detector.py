95453ea5c5240d5a7d01624d381b65dc70d6a143
GaneshDesk <koradeganesh.r@gmail.com>
2020-08-29 23:41:38 +0530

    The Script used for Detect duplicate files in Directory

---
+++ Python_Automation/Duplicate_File_Detector.py
@@ -0,0 +1,92 @@
+"""Automation script which accept directory name from user and display all names of
+duplicate files from that directory."""
+
+from sys import *
+import os
+import hashlib
+
+
+def hashfile(path, blocksize=1024):
+    fd = open(path, 'rb')
+    hasher = hashlib.md5()
+    buf = fd.read()
+
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = fd.read(blocksize)
+
+    fd.close()
+
+    return hasher.hexdigest()
+
+
+def FindDuplicate(path):
+    flag = os.path.isabs(path)
+
+    if flag == False:
+        path = os.path.abspath(path)
+
+    exists = os.path.isdir(path)
+
+    dups = {}
+    if exists:
+        for dirName, subdir, fileList in os.walk(path):
+            for filen in fileList:
+                path = os.path.join(dirName, filen)
+                file_hash = hashfile(path)
+                if file_hash in dups:
+                    dups[file_hash].append(path)
+                else:
+                    dups[file_hash] = [path]
+
+        return dups
+    else:
+        print("Invalid path")
+
+
+def PrintDuplicate(dict1):
+    results = list(filter(lambda x: len(x) > 1, dict1.values()))
+
+    if len(results) > 0:
+        print("Duplicate found : ")
+
+        print("Following files are identical : ")
+
+        icnt = 0
+        for result in results:
+            for subresult in result:
+                icnt += 1
+                if icnt >= 2:
+                    print('\t\t%s' % subresult)
+    else:
+        print("No duplicate file found.")
+
+
+def main():
+    print("Duplicate file Detector application .")
+
+    print("Application Name : "+argv[0])
+
+    if(len(argv) != 2):
+        print("Error : Invalid number of arguments .")
+        exit()
+
+    if(argv[1] == '-h')or(argv[1] == '-H'):
+        print("This Script is used to traverse the specific directory and display size of files.")
+        exit()
+
+    if(argv[1] == '-u')or(argv[1] == '-U'):
+        print("usage : ApplicationName AbsolutePath_Path_Of_Directory Extension.")
+        exit()
+
+    try:
+        arr = {}
+        arr = FindDuplicate(argv[1])
+        PrintDuplicate(arr)
+
+    except ValueError:
+        print("Error : Invalid datatype of input.")
+
+
+if __name__ == "__main__":
+    main()
